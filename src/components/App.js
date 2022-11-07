ff4394cd1fde1c37784e9bead158652b89aebe2f
StuartCowley <voidstalker@live.co.uk>
2022-11-07 13:29:02 +0000

    Introduce Beerlist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/app.css";
 
+import BeerList from "./BeerList";
+
 const App = () => {
   return (
     <div className="app">
       App
+      <BeerList />
     </div>
   );
 }
--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/beerlist.css";
 
+import Beer from "./Beer";
+
 const BeerList = () => {
   return (
     <div className="beerlist">
       Beerlist: Wrapper component to display list of beers
+      <Beer />
+      <Beer />
     </div>
   );
 };
