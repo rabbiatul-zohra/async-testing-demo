f16faf1c3bcf5551dc921281fd0da995d8afeaab
StuartCowley <voidstalker@live.co.uk>
2022-11-07 13:31:02 +0000

    use axios to get a random beer on component mount

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/app.css";
+import axios from "axios";
 
 import BeerList from "./BeerList";
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get("https://api.punkapi.com/v2/beers/random")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log("Server error: " + err));
+  }, []);
   return (
     <div className="app">
       App
       <BeerList />
     </div>
   );
-}
+};
 
 export default App;
