08c54645f63d95b4475d958e301654dd3c33cb58
StuartCowley <voidstalker@live.co.uk>
2022-11-07 11:53:00 +0000

    remove boilerplate and scaffold project, install axios

---
+++ src/components/Beer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/beer.css";
+
+const Beer = () => {
+  return <div className="beer">Beer: An instance of a single beer</div>;
+};
+
+export default Beer;
---
+++ src/components/BeerList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/beerlist.css";
+
+const BeerList = () => {
+  return (
+    <div className="beerlist">
+      Beerlist: Wrapper component to display list of beers
+    </div>
+  );
+};
+
+export default BeerList;
