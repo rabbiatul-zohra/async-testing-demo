25a048c446211c89c9b1ee36f3d74605205e338c
StuartCowley <voidstalker@live.co.uk>
2023-03-28 18:12:18 +0100

    use jest.mock and mockResolvedValue to test api

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "../components/App";
+import getBeer from "../requests/getBeer";
+
+jest.mock("../requests/getBeer");
 
 describe("App", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("renders app title", () => {
     render(<App />);
     const titleElement = screen.getByText(/Random beer generator/i);
     expect(titleElement).toBeInTheDocument();
   });
-})
+
+  test("component renders with api results", async () => {
+    getBeer.mockResolvedValue({
+      name: "test beer name",
+      tagline: "test tagline",
+      image_url: "https://dummy-test-url.com/",
+    });
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText("test beer name")).toBeInTheDocument();
+    });
+  });
+});
