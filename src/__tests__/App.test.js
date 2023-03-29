e94381c761d4ff9d8ad9e72547e3e5f5f90df5c1
StuartCowley <voidstalker@live.co.uk>
2023-03-29 13:33:10 +0100

    Use jest.spyOn to mock response

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
+import axios from "axios";
 
 describe("App", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("renders app title", () => {
     render(<App />);
     const titleElement = screen.getByText(/Random beer generator/i);
     expect(titleElement).toBeInTheDocument();
   });
-})
+
+  test("renders content from api", async () => {
+    const mAxiosResponse = {
+      data: [
+        {
+          name: "test beer name",
+          tagline: "test tagline",
+          image_url: "https://dummy-test-url.com/",
+        },
+      ],
+    };
+    jest.spyOn(axios, "get").mockResolvedValue(mAxiosResponse);
+    render(<App />);
+
+    expect(await screen.findByText("test beer name")).toBeInTheDocument();
+  });
+});
